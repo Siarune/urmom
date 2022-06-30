9c4fc9119a3bf6eb70a5cf4f121e17d2fc7e9914
Aidan Sharp <mhofutheweirdo@gmail.com>
2022-06-30 16:52:14 -0400

    added quotes tab

---
+++ components/Quotes.tsx
@@ -0,0 +1,99 @@
+import styles from "../styles/tabby.module.sass"
+
+
+export default function Quotes() {
+    return(
+        <div className={styles.texty}>
+            <h1>Weird quotes from Emily (usually before passing out)</h1>
+            <ul className={styles.list}>
+                <li>
+                They had a cat hair in their nose but didn&apos;t want to move their to get out and kept saying I was sweet when I offered to get it for them even though we live miles away from each other.
+                </li>
+                <br />
+
+                <li>
+                *Me, typing* <br />
+                &quot;Tappy tappy tappy tappy tappy tappy tappy tappy tappy&quot;
+                </li>
+                <br />
+
+                <li>
+                &quot;You&apos;re so cute, but so gay
+                &quot;</li>
+                <br />
+
+                <li>
+                &quot;Oh my God I just saw your face in the rocks
+                &quot;</li>
+                <br />
+
+                <li>
+                Would die happy after eating a chicken sandwich dipped in nacho cheese
+                </li>
+                <br />
+
+                <li>
+                Didn&apos;t know about Norway when they were little so they thought Norwegians lived in Norwegia and went on a rant about the name of Norway.
+                </li>
+                <br />
+
+                <li>
+                Lecture about vaginal health and couldn&apos;t remember what one thing was called so spent 10 minutes looking it up before continuing
+                </li>
+                <br />
+
+                <li>
+                &quot;You should go to sleep soon baby&quot; <br />
+                &quot;No I don&apos;t&quot; <br />
+                &quot;Why&apos;s that? &quot; <br />
+                &quot;Oprah&apos;s still alive... Isn&apos;t she? &quot;
+                </li>
+                <br />
+
+                <li>
+                &quot;Do you ever just like think? Why is a little squishy thing (brain) so smart?  That&apos;s fucked up. I feel like the smartest part of the body shouldn&apos;t be the brain, it should be the toes. Wouldn&apos;t it be cool if your toes could think for themselves, or smell where you&apos;re going.&quot; <br />
+                Proceeds to talk about cutting holes in toes so all the nerve ending can smell stuff and how we could evolve like moles.
+                </li>
+                <br />
+
+                <li>
+                Listed every single thing they could make in genshin (for at least five minutes)
+                </li>
+                <br />
+
+                <li>
+                Singing &quot;you are a frog&quot; like YMCA
+                </li>
+                <br />
+
+                <li>
+                &quot;Do you like my smolder? I think I look like Flynn Rider&quot;
+                </li>
+                <br />
+
+                <li>
+                Emo and brocken~~~~ <br />
+                (No, that is not a typo)
+                </li>
+                <br />
+
+                <li>
+                &quot;I have to go brush my teeth.&quot; <br />
+                &quot;I have something for you to brush your teeth with.&quot; <br />
+                Refused to explain what that meant
+                </li>
+                <br />
+
+                <li>
+                If they could choose how to die they&apos;d spontaneously burst/explode
+                </li>
+                <br />
+
+                <li>
+                &quot;I&apos;m high as a giraffe&apos;s pussy&quot;
+                </li>
+
+            </ul>
+        </div>
+    )
+}
