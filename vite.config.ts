0dbb467903087c17f9897f77723035ee095a4351
siarune <aidan.sharp@siarune.dev>
2023-09-26 16:07:22 -0400

    ISR support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import solid from "solid-start/vite"
 import vercel from "solid-start-vercel"
 
 export default defineConfig({
-// @ts-ignore
-	plugins: [solid({ adapter: vercel() })]
+	plugins: [
+		solid({
+			ssr: true,
+			adapter: vercel({
+				// @ts-ignore
+				prerender: true,
+			}),
+		}),
+	],
 })
