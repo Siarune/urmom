f90560e4e342998f586fee0c29e6c65739d59f7a
siarune <aidan.sharp@siarune.dev>
2024-05-10 11:28:46 -0400

    vercel db, unocss

--- src/routes/quotes/index.tsx
+++ src/routes/quotes/index.tsx
@@ -1,48 +1,57 @@
-import { desc } from "drizzle-orm"
-import { For } from "solid-js"
-import { useRouteData } from "solid-start"
-import { Form } from "solid-start/data/Form"
-import { createServerAction$, createServerData$ } from "solid-start/server/server"
+import { action, cache, createAsync } from "@solidjs/router"
+import { desc, eq } from "drizzle-orm"
+import { Suspense } from "solid-js"
 import db, { quotes } from "~/db"
-import styles from "~/routes/quotes/index.module.sass"
 
-export function routeData() {
-	return createServerData$(() =>
-		db.select()
-			.from(quotes)
-			.orderBy(desc(quotes.id))
-			.limit(500)
-	)
+const getQuotes = cache(async () => {
+	"use server";
+
+	return db
+		.select({
+			id: quotes.id,
+			content: quotes.content
+		})
+		.from(quotes)
+		.where(eq(quotes.public, true))
+		.orderBy(desc(quotes.id))
+		.limit(500)
+
+}, "quotes")
+
+const createQuote = action(async ( formData: FormData ) => {
+	"use server";
+
+	await new Promise(( resolve, reject ) => {
+		db
+			.insert(quotes)
+			.values({
+				//@ts-ignore
+				content: formData.get("content")?.toString()
+			})
+			.then(resolve)
+		reject()
+	})
+})
+
+export const route = {
+	load: () => getQuotes()
 }
 
 export default function Quotes() {
+	const Quotes = createAsync(() => getQuotes())
 
-	const [_, { Form }] =
-		createServerAction$(async ( form: FormData ) => {
-			const Content = await form.get("content") as string
-			await db.insert(quotes).values({ content: Content })
-			return (() => console.log("success"))
-		})
+	return (<main class="min-h-100vh max-h-fit flex flex-col items-center bg-black color-white">
 
-	const Quotes = useRouteData<typeof routeData>()
-
-	return (
-		<main class={styles.main}>
-
-			<Form class={styles.form}>
-				<input name="content" id="content" type="text" />
-				<input type="submit" value={"Send it!"} />
-			</Form>
-
-			<ul class={styles.list}>
-				<For each={Quotes()}>
-					{( quote ) =>
-						<li>
-							{quote.content}
-						</li>
-					}
-				</For>
-			</ul>
-		</main>
-	)
+		<form action={createQuote} method="post" class="m5">
+			<input required type="text" name="content" class="color-black text-2xl" />
+			<input type="submit" value="Send it!" class="p3 hover:cursor-pointer" />
+		</form>
+		<ul class="text-1.5rem mb0 w90vw sm:w50vw">
+			<Suspense fallback={<></>}>
+				{Quotes() && Quotes()!.map(( quote ) =>
+					<li class="list-none pb6">{quote.content}</li>
+				)}
+			</Suspense>
+		</ul>
+	</main>)
 }
