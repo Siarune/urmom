54d1d2fd54b30bd0e73ec7f92381c586dc908907
siarune <aidan.sharp@siarune.dev>
2025-03-16 18:23:58 -0400

    share vercel

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,11 @@
-import { drizzle } from "drizzle-orm/node-postgres"
-import { pgTable, serial, text } from "drizzle-orm/pg-core"
+import { drizzle } from "drizzle-orm/vercel-postgres"
+import { serial, text, pgTableCreator } from "drizzle-orm/pg-core"
 
-const db = drizzle(process.env.DB_URL!)
+// const db = drizzle(process.env.DB_URL!)
+const db = drizzle()
 export default db
+
+const pgTable = pgTableCreator((name) => `urmom_${name}`);
 
 export const quotes = pgTable("quotes", {
 	id: serial().primaryKey(),
