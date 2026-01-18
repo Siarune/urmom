c55221a919ae0479cd9ec5c860712eede354e829
siarune <asharp@siarune.dev>
2026-01-17 20:41:27 -0500

    add catwife page

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,17 @@
 import { drizzle } from "drizzle-orm/vercel-postgres"
 import { serial, text, pgTableCreator } from "drizzle-orm/pg-core"
 
-// const db = drizzle(process.env.DB_URL!)
 const db = drizzle()
 export default db
 
-const pgTable = pgTableCreator((name) => `urmom_${name}`);
+const pgTable = pgTableCreator((name) => `urmom_${name}`)
 
 export const quotes = pgTable("quotes", {
+	id: serial().primaryKey(),
+	content: text().notNull(),
+})
+
+export const cwt = pgTable("catwifetheory", {
 	id: serial().primaryKey(),
 	content: text().notNull(),
 })
---
+++ src/routes/catwife_theory.tsx
@@ -0,0 +1,93 @@
+import { action, createAsync, query } from "@solidjs/router"
+import db, { cwt } from "~/db"
+import { desc } from "drizzle-orm"
+import { Suspense } from "solid-js"
+import { SolidMarkdown } from "solid-markdown"
+
+export default function CWT() {
+	let dialogRef!: HTMLDialogElement
+	const Quotes = createAsync(() => getQuotes())
+
+	return (
+		<main class="flex flex-col w-full min-h-screen max-h-fit bg-black justify-center items-center content-center">
+			<dialog
+				ref={dialogRef}
+				class="rounded"
+			>
+				<form
+					action={insertQuote}
+					method="post"
+					class="flex flex-col m2"
+				>
+					<input
+						required
+						autofocus
+						type="text"
+						name="content"
+						class="color-black text-2xl"
+					/>
+					<div class="flex flex-row justify-around">
+						<button
+							onClick={() => dialogRef.close()}
+							class=""
+						>
+							Cancel
+						</button>
+						<input
+							onclick={() => dialogRef.close()}
+							type="submit"
+							value="Send it!"
+							class="p3 hover:cursor-pointer"
+						/>
+					</div>
+				</form>
+			</dialog>
+
+			<button
+				onClick={() => dialogRef.showModal()}
+				class="aspect-ratio-square rounded-full bg-emerald-600 color-white font-extrabold text-8 p4 m0
+                inline-flex text-center flex-col justify-center items-center align-mid
+                pos-fixed pos-bottom-sm pos-right-sm"
+			>
+				+
+			</button>
+
+			<ul
+				class="text-1.5rem mb0 w90vw sm:w50vw text-white"
+				// style="white-space: wrap;"
+			>
+				<Suspense fallback={<></>}>
+					{Quotes() &&
+						Quotes()!.map((quote) => (
+							<li class="list-none pb6">
+								<SolidMarkdown renderingStrategy="reconcile">
+									{quote.content}
+								</SolidMarkdown>
+							</li>
+						))}
+				</Suspense>
+			</ul>
+		</main>
+	)
+}
+
+const getQuotes = query(async () => {
+	"use server"
+	return db
+		.select({
+			content: cwt.content,
+		})
+		.from(cwt)
+		.orderBy(desc(cwt.id))
+		.limit(500)
+}, "quotes")
+
+const insertQuote = action(async (formData: FormData) => {
+	"use server"
+	await db
+		.insert(cwt)
+		.values({
+			content: formData.get("content")!.toString(),
+		})
+		.returning()
+})
