fbfc82ce9af8e81e417f4879abc9fb3b9f0f30c8
siarune <asharp@siarune.dev>
2026-01-17 21:45:09 -0500

    dep cleanup

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "@solidjs/start/config"
 import UnoCSS from "unocss/vite"
-import devtools from "solid-devtools/vite"
-// import oxlintPlugin from "vite-plugin-oxlint"
 
 export default defineConfig({
 	vite: {
-		plugins: [
-			devtools({
-				autoname: true,
-			}),
-			UnoCSS(),
-			// oxlintPlugin(),
-		],
+		plugins: [UnoCSS()],
 	},
 })
